5cf0e6c2f9a266ac48787da371cebc593b37bf0d
kseniia <kseniia@smartiot.cloud>
2022-10-06 18:23:03 +0300

    add rules for calculating integrals using 3 different methods

---
+++ src/models/integralRule.js
@@ -0,0 +1,94 @@
+class IntegralRule {
+  constructor(interval, stepsCount) {
+    this.interval = interval;
+    this.stepsCount = stepsCount;
+
+    this.delta = this.calculateStep();
+  }
+
+  calculateStep() {
+    const {start, end} = this.interval;
+
+    return (end - start) / this.stepsCount;
+  }
+
+  calculateIntervalPoints() {}
+
+  calculate() {}
+}
+
+class MidpointRule extends IntegralRule {
+  calculateIntervalPoints() {
+    const midPoints = [];
+    const {start, end} = this.interval;
+
+    for (let i = start; i + this.delta <= end; i += this.delta) {
+      const curStart = i;
+      const curEnd = i + this.delta;
+      midPoints.push((curEnd + curStart) / 2);
+    }
+
+    return midPoints;
+  }
+
+  calculate(func) {
+    const midPoints = this.calculateIntervalPoints();
+    return midPoints.reduce((sum, currentPoint) => {
+      return sum + this.delta * func(currentPoint);
+    }, 0);
+  }
+}
+
+class TrapezoidRule extends IntegralRule {
+  calculateIntervalPoints() {
+    const intervalPoints = [];
+    const {start, end} = this.interval;
+
+    for (let i = start; i <= end; i += this.delta) {
+      intervalPoints.push(i);
+    }
+
+    return intervalPoints;
+  }
+
+  calculate(fX) {
+    const intervalPoints = this.calculateIntervalPoints();
+    const lastIndex = intervalPoints.length - 1;
+    return intervalPoints.reduce((sum, currentPoint, index) => {
+      let funcExpr = 2 * fX(currentPoint);
+      if (index === 0 || index === lastIndex) {
+        funcExpr = fX(currentPoint);
+      }
+      return sum + 0.5 * this.delta * funcExpr;
+    }, 0);
+  }
+}
+
+class SimpsonRule extends IntegralRule {
+  calculateIntervalPoints() {
+    const intervalPoints = [];
+    const {start, end} = this.interval;
+
+    for (let i = start; i <= end; i += this.delta) {
+      intervalPoints.push(i);
+    }
+
+    return intervalPoints;
+  }
+
+  calculate(fX) {
+    const intervalPoints = this.calculateIntervalPoints();
+    const lastIndex = intervalPoints.length - 1;
+    return intervalPoints.reduce((sum, currentPoint, index) => {
+      let funcExpr = 2 * fX(currentPoint);
+      if (index === 0 || index === lastIndex) {
+        funcExpr = fX(currentPoint);
+      } else if (index % 2 !== 0) {
+        funcExpr = 4 * fX(currentPoint);
+      }
+      return sum + (this.delta / 3) * funcExpr;
+    }, 0);
+  }
+}
+
+module.exports = {MidpointRule, TrapezoidRule, SimpsonRule};
