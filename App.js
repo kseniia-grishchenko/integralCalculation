a5cb048f80196d7bc9b5b2bf9ea1e237fad309c2
kseniia <kseniia@smartiot.cloud>
2022-10-07 23:13:11 +0300

    include them in root

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import FunctionCheckbox from './src/components/functionCheckbox';
-import CoefficientsInput from './src/components/coefficientsInput';
+import FunctionCheckbox from './src/components/functionCheckbox.js';
+import CoefficientsInput from './src/components/coefficientsInput.js';
+import IntegralCalculator from './src/components/integralCalculator.js';
+import CalculationMethodCheckbox from './src/components/calculationMethodCheckbox.js';
+import IntervalInput from './src/components/intervalInput.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +16,15 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Choose function" component={FunctionCheckbox} />
         <Stack.Screen name="Coefficients input" component={CoefficientsInput} />
+        <Stack.Screen name="Interval input" component={IntervalInput} />
+        <Stack.Screen
+          name="Choose calculation rule"
+          component={CalculationMethodCheckbox}
+        />
+        <Stack.Screen
+          name="Integral calculator"
+          component={IntegralCalculator}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
