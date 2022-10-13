72ff40dae03076b576eaf1039f43fcbd77a457a8
kseniia <kseniia@smartiot.cloud>
2022-10-13 11:18:20 +0300

    don't need this component

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FunctionCheckbox from './src/components/functionCheckbox.js';
 import CoefficientsInput from './src/components/coefficientsInput.js';
 import IntegralCalculator from './src/components/integralCalculator.js';
-import CalculationMethodCheckbox from './src/components/calculationMethodCheckbox.js';
 import IntervalInput from './src/components/intervalInput.js';
 
 const Stack = createNativeStackNavigator();
@@ -17,10 +16,6 @@ const App = () => {
         <Stack.Screen name="Choose function" component={FunctionCheckbox} />
         <Stack.Screen name="Coefficients input" component={CoefficientsInput} />
         <Stack.Screen name="Interval input" component={IntervalInput} />
-        <Stack.Screen
-          name="Choose calculation rule"
-          component={CalculationMethodCheckbox}
-        />
         <Stack.Screen
           name="Integral calculator"
           component={IntegralCalculator}
