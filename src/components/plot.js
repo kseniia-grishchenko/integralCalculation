789e61b3679d4cebfdbe4f4a3cc2ec0a74be0382
kseniia <kseniia@smartiot.cloud>
2022-10-18 23:44:46 +0300

    rm unused component

--- src/components/plot.js
+++ src/components/plot.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, View, StyleSheet, Text} from 'react-native';
+import {Dimensions, View, StyleSheet} from 'react-native';
 import {LineChart} from 'react-native-chart-kit';
 
 const Plot = ({xValues, yValues}) => {
