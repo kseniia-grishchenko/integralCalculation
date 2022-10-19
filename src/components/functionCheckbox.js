9afc3a97213db1009a518974a55b573af83f7f15
kseniia <kseniia@smartiot.cloud>
2022-10-19 22:40:04 +0300

    tweak styles

--- src/components/functionCheckbox.js
+++ src/components/functionCheckbox.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+import ContinueBtn from './continueBtn.js';
 
 const functionChoices = ['parabolic', 'sinusoid'];
 
@@ -10,31 +11,32 @@ const FunctionCheckbox = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Choose function type</Text>
-      {functionChoices.map(functionChoice => (
-        <View style={styles.checkboxContainer} key={functionChoice}>
-          <CheckBox
-            boxType="square"
-            onFillColor="#3CA1FF"
-            onCheckColor="#fff"
-            onTintColor="#3CA1FF"
-            animationDuration="0,2"
-            value={selected === functionChoice}
-            onValueChange={value => setSelection(value ? functionChoice : null)}
-            style={styles.checkbox}
-          />
-          <Text style={styles.label}>{functionChoice}</Text>
-        </View>
-      ))}
+      <View style={styles.funcContainer}>
+        {functionChoices.map(functionChoice => (
+          <View style={styles.checkboxContainer} key={functionChoice}>
+            <CheckBox
+              boxType="square"
+              onFillColor="#3CA1FF"
+              onCheckColor="#fff"
+              onTintColor="#3CA1FF"
+              value={selected === functionChoice}
+              onValueChange={value =>
+                setSelection(value ? functionChoice : null)
+              }
+              style={styles.checkbox}
+            />
+            <Text style={styles.label}>{functionChoice}</Text>
+          </View>
+        ))}
+      </View>
       {selected && (
-        <TouchableOpacity
-          style={styles.btnContainer}
-          onPress={() => {
-            navigation.navigate('Coefficients input', {
-              functionChoice: selected,
-            });
-          }}>
-          <Text style={styles.btnText}>Continue</Text>
-        </TouchableOpacity>
+        <ContinueBtn
+          navigation={navigation}
+          screenName={'Coefficients input'}
+          props={{
+            functionChoice: selected,
+          }}
+        />
       )}
     </View>
   );
@@ -59,6 +61,9 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginRight: 12,
   },
+  funcContainer: {
+    marginBottom: 40,
+  },
   label: {
     textTransform: 'capitalize',
     fontFamily: 'Montserrat-Medium',
@@ -70,21 +75,6 @@ const styles = StyleSheet.create({
     fontSize: 40,
     textAlign: 'center',
     marginBottom: 40,
-  },
-  btnContainer: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#3CA1FF',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 14,
-    borderRadius: 10,
-    marginTop: 40,
-  },
-  btnText: {
-    fontFamily: 'Montserrat-SemiBold',
-    color: '#fff',
-    fontSize: 18,
   },
 });
 
