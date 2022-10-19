b2b92a6ad05c018a4aaeb36a7f646e237f339553
kseniia <kseniia@smartiot.cloud>
2022-10-19 20:57:43 +0300

    function checkbox page css

--- src/components/functionCheckbox.js
+++ src/components/functionCheckbox.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 
 const functionChoices = ['parabolic', 'sinusoid'];
@@ -9,9 +9,15 @@ const FunctionCheckbox = ({navigation}) => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Choose function type</Text>
       {functionChoices.map(functionChoice => (
         <View style={styles.checkboxContainer} key={functionChoice}>
           <CheckBox
+            boxType="square"
+            onFillColor="#3CA1FF"
+            onCheckColor="#fff"
+            onTintColor="#3CA1FF"
+            animationDuration="0,2"
             value={selected === functionChoice}
             onValueChange={value => setSelection(value ? functionChoice : null)}
             style={styles.checkbox}
@@ -20,14 +26,15 @@ const FunctionCheckbox = ({navigation}) => {
         </View>
       ))}
       {selected && (
-        <Button
-          title="Continue"
+        <TouchableOpacity
+          style={styles.btnContainer}
           onPress={() => {
             navigation.navigate('Coefficients input', {
               functionChoice: selected,
             });
-          }}
-        />
+          }}>
+          <Text style={styles.btnText}>Continue</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
@@ -38,17 +45,46 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#fff',
   },
   checkboxContainer: {
     flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginBottom: 20,
+    gap: 12,
   },
   checkbox: {
     alignSelf: 'center',
+    marginRight: 12,
   },
   label: {
-    margin: 8,
     textTransform: 'capitalize',
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 18,
+    color: '#646464',
+  },
+  title: {
+    fontFamily: 'Montserrat-ExtraBold',
+    fontSize: 40,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  btnContainer: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#3CA1FF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 14,
+    borderRadius: 10,
+    marginTop: 40,
+  },
+  btnText: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#fff',
+    fontSize: 18,
   },
 });
 
